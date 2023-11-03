9b73835885796fba8b4b646d443466f1ec75d0d8
Clarice Bascou <clarice.bascou@gmail.com>
2023-11-03 11:43:34 +0100

    correction year

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('https://whatyearisit-backend-nu-coral.vercel.app')
+fetch('https://whatyearisit-backend-nu-coral.vercel.app/year')
  .then(response => response.json())
  .then(data => {
     const year = data.year;

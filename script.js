1eb3299fed6853fafcc86ba06fae54afdf3c6c1e
Clarice Bascou <clarice.bascou@gmail.com>
2023-11-03 11:53:43 +0100

    toString

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ fetch('https://whatyearisit-backend-nu-coral.vercel.app/year')
  .then(response => response.json())
  .then(data => {
     const year = data.year;
-    document.querySelector('#year').textContent = year;
+    document.querySelector('#year').textContent = year.toString();
  });
